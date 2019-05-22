3037e88bb20df63333bfa9c286f775dee58397dd
John Dros <johndrosoulakis@gmail.com>
2019-05-22 13:07:17 +0300

    build structure

--- mtg-app/src/app/main/main.module.ts
+++ mtg-app/src/app/main/main.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
+import { NavComponent } from './layouts/nav/nav.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { ContentComponent } from './layouts/content/content.component';
+import { CardListComponent } from './cards/card-list/card-list.component';
+import { CardDetailComponent } from './cards/card-detail/card-detail.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavComponent, FooterComponent, ContentComponent, CardListComponent, CardDetailComponent],
   imports: [
     CommonModule,
     MainRoutingModule,
