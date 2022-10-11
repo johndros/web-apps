4db559f227bf6324d38a6512b0cbb21c0c38f85e
JohnDros <johndrosoulakis@gmail.com>
2022-10-11 17:09:18 +0300

    Delete mtg-app directory

--- mtg-app/src/app/main/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './main.component';
-export * from './layouts/nav/nav.component';
-export * from './layouts/content/content.component';
-export * from './layouts/footer/footer.component';
-export * from './cards/card-list/card-list.component';
-export * from './cards/card-detail/card-detail.component';
-export * from './cards/pagination/pagination.component';
